5918643708a04f24685dff17fdcf6e498b9e8d6f
Remco Drolenga <89783539+RemcoDrolenga@users.noreply.github.com>
2023-01-11 10:21:21 +0100

    update

--- Gamepage.php
+++ Gamepage.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="style.css">
+        <?php 
+        require('function.php')
+        Connectdatabase()
+        ?>
     </head>
     <body>
         <header class="intro">
@@ -16,11 +20,6 @@
         </header>
         <div class="page-wrap">
         <img style="position:relative; left:510px; top:10px" src="images/GTA.webp" alt="qwixx" width="225" height="250">
-        <!-- <img style="position:relative; left:280px; top:250px;" src="images/littlebigplanet3.jpg"
-        alt="mens-erger-je-niet" width="200" height="220">
-        <img style="position:relative; left:78px; top:510px;" src="images/horizonforbiddenwest.webp"
-        alt="doolhof" width="200" height="240">
-        <div class="push"></div> -->
    
     <h4 class="summary">Grand Theft Auto 5<br><br><br> Ga voor de ultieme GTA V-ervaring met de<br> GTA V Premium Edition voor de PlayStation 4!<br> 
         De GTA V Premium Edition bevat het Criminal<br> Enterprise Starter Pack met onder meer<br> 1.000.000 in-game valuta.</h4>
--- functions.php
+++ functions.php
@@ -1,25 +1,24 @@
 <?php
 
-function Connectdatabase()
+function Connectdatabase(){
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "mareal";
     try {
-     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $stmt = $conn->prepare("SELECT id , min_players, max_players, play_minutes, explain_minutes, name, image, description FROM games");
-     $stmt->execute();
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+     }catch (PDOException $exception){
+        exit("failed to connect")
      }
-
- catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
 }
 
-$conn = null;
-echo "</table>";
-$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach($stmt->fetchAll() as $v) {
-    echo "<a id= ".$v["id"]. "src=images/" .$v["image"] ."></a>";
 
+// $conn = null;
+// echo "</table>";
+// $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+//   foreach($stmt->fetchAll() as $v) {
+    // echo "<a id= ".$v["id"]. "src=images/" .$v["image"] ."></a>";
+//   }
 ?> 
